68b24199966868889ef8c79153c68f9f66ddf65c
Rukmangathan <prukmangathan@gmail.com>
2022-09-27 15:05:43 +0530

    Actions

--- functions_code/demo-functions/main.py
+++ functions_code/demo-functions/main.py
@@ -1,24 +1,11 @@
-import functions_framework
+import base64
 
-@functions_framework.http
-def hello_http(request):
-   """HTTP Cloud Function.
-   Args:
-       request (flask.Request): The request object.
-       <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-   Returns:
-       The response text, or any set of values that can be turned into a
-       Response object using `make_response`
-       <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-   """
-   request_json = request.get_json(silent=True)
-   request_args = request.args
-
-   if request_json and 'name' in request_json:
-       name = request_json['name']
-   elif request_args and 'name' in request_args:
-       name = request_args['name']
-   else:
-       name = 'World'
-   return 'Hello {}!'.format(name)
-
+def hello_pubsub(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+         TEST
+    """
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    print(pubsub_message)
